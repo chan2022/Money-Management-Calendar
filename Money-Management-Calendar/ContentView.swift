ed06465a06c66be10fdfd256c08c42d3dea47e0b
Erica Han <Erica.Han@morningstar.com>
2024-04-08 10:16:21 -0500

    initial commit

---
+++ Money-Management-Calendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Money-Management-Calendar
+//
+//  Created by Erica Han on 4/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Money-Management-Calendar/Money_Management_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Money_Management_CalendarApp.swift
+//  Money-Management-Calendar
+//
+//  Created by Erica Han on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Money_Management_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
